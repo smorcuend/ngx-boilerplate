b7f6008fb30c2ef6a0f6fe19e0b899fe3ceb43e9
Sergio <sergio@seedtag.com>
2016-11-25 11:31:49 +0100

    Angular2DataTableModule removed

--- src/app/app.vendors.ts
+++ src/app/app.vendors.ts
@@ -1,11 +1,10 @@
 // Vendors
 import { SlimLoadingBarComponent } from 'ng2-slim-loading-bar';
-import { Angular2DataTableModule } from 'angular2-data-table';
 
 export const VENDOR_DECLARATIONS: any = [
   SlimLoadingBarComponent
 ];
 
 export const VENDOR_MODULES: any = [
-  Angular2DataTableModule
+
 ];
